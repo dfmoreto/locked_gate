f33816a50d94713488bc6b516975d133787bb856
Daniel Moreto <dfmoreto@gmail.com>
2022-05-11 15:06:49 -0300

    Add methods to customize configs for each part LockedGate is included

--- lib/locked_gate.rb
+++ lib/locked_gate.rb
@@ -1,6 +1,29 @@
-require "locked_gate/version"
-require "locked_gate/railtie"
+require 'locked_gate/version'
+require 'locked_gate/railtie'
+require 'locked_gate/configuration'
 
 module LockedGate
-  # Your code goes here...
+  def self.included(base)
+    base.extend ClassMethods
+  end
+
+  def self.configure(&block)
+    @configuration = Configuration.new
+    block.call(@configuration)
+  end
+
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
+
+  module ClassMethods
+    def gate_unlock_configuration(&block)
+      @custom_locked_gate_configuration = LockedGate.configuration
+      block.call(@custom_locked_gate_configuration)
+    end
+
+    def custom_locked_gate_configuration
+      @custom_locked_gate_configuration ||= LockedGate.configuration
+    end
+  end
 end
--- spec/locked_gate_spec.rb
+++ spec/locked_gate_spec.rb
@@ -2,4 +2,126 @@ RSpec.describe LockedGate do
   it 'has a version number' do
     expect(LockedGate::VERSION).to be_present
   end
+
+  describe 'default configuration' do
+    let(:locked_gate_class) { described_class.clone }
+
+    context 'when there are not config set' do
+      it ':post_param receive default value' do
+        expect(locked_gate_class.configuration.post_param).to eq :token
+      end
+
+      it ':query_string_param receive default value' do
+        expect(locked_gate_class.configuration.query_string_param).to eq :token
+      end
+
+      it ':expiration_param receive default value' do
+        expect(locked_gate_class.configuration.expiration_param).to eq :exp
+      end
+
+      it 'header_config :regex receive default value' do
+        expect(locked_gate_class.configuration.header_config.regex).to eq /Bearer (.*)\s?/
+      end
+
+      it 'header_config :match receive default value' do
+        expect(locked_gate_class.configuration.header_config.match).to eq '\1'
+      end
+    end
+
+    context 'when there are config set' do
+      before do
+        locked_gate_class.configure do |config|
+          config.post_key :test_post
+          config.query_string_key :query_string_test
+          config.expiration_key :expiration
+          config.header_regex regex: /my_regex/, match: '\3'
+        end
+      end
+
+      it ':post_param receive configured value' do
+        expect(locked_gate_class.configuration.post_param).to eq :test_post
+      end
+
+      it ':query_string_param receive configured value' do
+        expect(locked_gate_class.configuration.query_string_param).to eq :query_string_test
+      end
+
+      it ':expiration_param receive configured value' do
+        expect(locked_gate_class.configuration.expiration_param).to eq :expiration
+      end
+
+      it 'header_config :regex receive configured value' do
+        expect(locked_gate_class.configuration.header_config.regex).to eq(/my_regex/)
+      end
+
+      it 'header_config :match receive configured value' do
+        expect(locked_gate_class.configuration.header_config.match).to eq '\3'
+      end
+    end
+  end
+
+  describe 'including' do
+    context 'without custom configuration' do
+      let(:some_class) do
+        Class.new do
+          include LockedGate
+        end
+      end
+
+      it ':post_param receive default value' do
+        expect(some_class.custom_locked_gate_configuration.post_param).to eq :token
+      end
+
+      it ':query_string_param receive default value' do
+        expect(some_class.custom_locked_gate_configuration.query_string_param).to eq :token
+      end
+
+      it ':expiration_param receive default value' do
+        expect(some_class.custom_locked_gate_configuration.expiration_param).to eq :exp
+      end
+
+      it 'header_config :regex receive default value' do
+        expect(some_class.custom_locked_gate_configuration.header_config.regex).to eq /Bearer (.*)\s?/
+      end
+
+      it 'header_config :match receive default value' do
+        expect(some_class.custom_locked_gate_configuration.header_config.match).to eq '\1'
+      end
+    end
+
+    context 'with custom configuration' do
+      let(:some_class) do
+        Class.new do
+          include LockedGate
+
+          gate_unlock_configuration do |config|
+            config.post_key :custom_post_token
+            config.query_string_key :custom_query_string_token
+            config.expiration_key :custom_expiration
+            config.header_regex regex: /custom_regex/, match: '\2'
+          end
+        end
+      end
+
+      it ':post_param receive default value' do
+        expect(some_class.custom_locked_gate_configuration.post_param).to eq :custom_post_token
+      end
+
+      it ':query_string_param receive default value' do
+        expect(some_class.custom_locked_gate_configuration.query_string_param).to eq :custom_query_string_token
+      end
+
+      it ':expiration_param receive default value' do
+        expect(some_class.custom_locked_gate_configuration.expiration_param).to eq :custom_expiration
+      end
+
+      it 'header_config :regex receive default value' do
+        expect(some_class.custom_locked_gate_configuration.header_config.regex).to eq /custom_regex/
+      end
+
+      it 'header_config :match receive default value' do
+        expect(some_class.custom_locked_gate_configuration.header_config.match).to eq '\2'
+      end
+    end
+  end
 end
