b3ccf8fa85ec8913115d61aa10b53a98b24c52dc
Daniel Moreto <dfmoreto@gmail.com>
2022-05-11 23:28:23 -0300

    Create JWT Auth Strategy

---
+++ lib/locked_gate/auth_strategies/jwt.rb
@@ -0,0 +1,36 @@
+require 'jwt'
+require_relative './base'
+require_relative '../authentication_error'
+
+module LockedGate
+  module AuthStrategies
+    class JWT < Base
+      def authenticate!
+        validate_expiration!
+      rescue ::JWT::DecodeError
+        raise AuthenticationError.new(:invalid_token, 'Invalid token')
+      end
+
+      def expiration
+        expiration_key = configuration.expiration_param.to_s
+        Time.zone.at(decoded_token.first[expiration_key])
+      end
+
+      def user_data
+        { email: decoded_token.first['email'] }
+      end
+
+      private
+
+      def validate_expiration!
+        raise AuthenticationError.new(:expired_token, 'Token expired') if expiration < Time.zone.now
+      end
+
+      def decoded_token
+        return @decoded_token if @decoded_token.present?
+
+        @decoded_token = ::JWT.decode(@token, nil, false)
+      end
+    end
+  end
+end
---
+++ spec/locked_gate/auth_strategies/jwt_spec.rb
@@ -0,0 +1,42 @@
+require 'locked_gate/authentication_error'
+
+describe LockedGate::AuthStrategies::JWT do
+  let(:token_expiration) { Time.zone.at(2.hours.from_now.to_i) }
+  let(:configuration) { LockedGate::Configuration.new }
+  let(:jwt_token) { JWT.encode({ email: 'test@test.com', exp: Integer(token_expiration) }, '123456', 'HS256') }
+
+  describe '#authenticate!' do
+    let(:expired_jwt_token) { JWT.encode({ email: 'test@test.com', exp: 2.minutes.ago.to_i }, '123456', 'HS256') }
+
+    it 'raises AuthenticationError when token is invalid' do
+      auth = described_class.new(configuration, 'some_invalid_token')
+      expect do
+        auth.authenticate!
+      end.to raise_error(LockedGate::AuthenticationError)
+    end
+
+    it 'raises AuthenticationError whem token is expired' do
+      auth = described_class.new(configuration, expired_jwt_token)
+      expect do
+        auth.authenticate!
+      end.to raise_error(LockedGate::AuthenticationError)
+    end
+
+    it 'does not raise any error when token is valid' do
+      auth = described_class.new(configuration, jwt_token)
+      expect do
+        auth.authenticate!
+      end.not_to raise_error
+    end
+  end
+
+  it '#expiration return expiration date' do
+    auth = described_class.new(configuration, jwt_token)
+    expect(auth.expiration).to eq token_expiration
+  end
+
+  it '#user_data returns hash with user fields' do
+    auth = described_class.new(configuration, jwt_token)
+    expect(auth.user_data).to eq({ email: 'test@test.com' })
+  end
+end
